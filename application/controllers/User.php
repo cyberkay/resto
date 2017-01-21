5a8c0fd04f050974806bad56dde2ab5a63c8b9a6
kiki kiswanto <kiki.kiswanto@hellobisnis.com>
2017-01-21 16:55:22 +0700

    create controller user

---
+++ application/controllers/User.php
@@ -0,0 +1,19 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+* Controller User
+*/
+class User extends CI_Controller
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public function login($redirect='')
+	{
+		echo "login";
+	}
+}
+
