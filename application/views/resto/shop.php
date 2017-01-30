878c0b08fd4e4acd2b0968cb9bd103c1fbded597
kiki kiswanto <kiki.kiswanto@hellobisnis.com>
2017-01-30 10:01:28 +0700

    add page resto

---
+++ application/views/resto/shop.php
@@ -0,0 +1,113 @@
+	<div class="col-md-2">
+	<?php $this->load->view(THEME . 'layout/menus_secondary');  ?>
+	</div>
+
+	<div class="col-md-7">
+
+	  <div class="col-md-4">
+	    <div class="thumbnail">
+	      <img src="<?= base_url('assets/images/menus/242x200.svg'); ?>" alt="Image 242x200">
+	      <div class="caption">
+	        <h3>Menu of name</h3>
+	        <p> Description Of menus </p>
+	        <p><a href="#" class="btn btn-block btn-primary" role="button">Add</a></p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-md-4">
+	    <div class="thumbnail">
+	      <img src="<?= base_url('assets/images/menus/242x200.svg'); ?>" alt="Image 242x200">
+	      <div class="caption">
+	        <h3>Menu of name</h3>
+	        <p> Description Of menus </p>
+	        <p><a href="#" class="btn btn-block btn-primary" role="button">Add</a></p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-md-4">
+	    <div class="thumbnail">
+	      <img src="<?= base_url('assets/images/menus/242x200.svg'); ?>" alt="Image 242x200">
+	      <div class="caption">
+	        <h3>Menu of name</h3>
+	        <p> Description Of menus </p>
+	        <p><a href="#" class="btn btn-block btn-primary" role="button">Add</a></p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-md-4">
+	    <div class="thumbnail">
+	      <img src="<?= base_url('assets/images/menus/242x200.svg'); ?>" alt="Image 242x200">
+	      <div class="caption">
+	        <h3>Menu of name</h3>
+	        <p> Description Of menus </p>
+	        <p><a href="#" class="btn btn-block btn-primary" role="button">Add</a></p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-md-4">
+	    <div class="thumbnail">
+	      <img src="<?= base_url('assets/images/menus/242x200.svg'); ?>" alt="Image 242x200">
+	      <div class="caption">
+	        <h3>Menu of name</h3>
+	        <p> Description Of menus </p>
+	        <p><a href="#" class="btn btn-block btn-primary" role="button">Add</a></p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-md-4">
+	    <div class="thumbnail">
+	      <img src="<?= base_url('assets/images/menus/242x200.svg'); ?>" alt="Image 242x200">
+	      <div class="caption">
+	        <h3>Menu of name</h3>
+	        <p> Description Of menus </p>
+	        <p><a href="#" class="btn btn-block btn-primary" role="button">Add</a></p>
+	      </div>
+	    </div>
+	  </div>
+
+	</div> <!-- end of main content -->
+
+	<div class="col-md-3">
+	
+	<div class="panel panel-default">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">List of Order <p class="pull-right">No. P212</p></h3>
+	  </div>
+	  <div class="panel-body">
+	    User : Guest
+	  	<p class="pull-right"><?= date("Y-m-d H:i:s") ?></p>
+	  	<hr>
+	  	<div class="row">
+	  		<div class="col-md-6">Menu x 2 </div>
+	  		<div class="col-md-6" align="right">Rp 40.000,-</div>
+	  	</div>
+	  	<div class="row">
+	  		<div class="col-md-6">Drinks x 2 </div>
+	  		<div class="col-md-6" align="right">Rp 20.000,-</div>
+	  	</div>
+	  	<div class="row">
+	  		<div class="col-md-6">Rice x 3 </div>
+	  		<div class="col-md-6" align="right">Rp 25.000,</div>
+	  	</div>
+	  	<hr>
+	  	<div class="row">
+	  		<div class="col-md-6">Sub total</div>
+	  		<div class="col-md-6" align="right">Rp 85.000,-</div>
+	  	</div>
+	  	<div class="row">
+	  		<div class="col-md-6">Tax 10%</div>
+	  		<div class="col-md-6" align="right">Rp 8.500,-</div>
+	  	</div>
+	  </div>
+	  <div class="panel-footer">
+	  	Total :
+	  	<p class="pull-right">Rp 93.500,-</p>
+	  </div>
+	</div>
+	<p><a href="#" class="btn btn-block btn-primary" role="button">Checkout</a></p>
+	</div> <!-- end of left widget -->
